43eaa236de55a01947920e69296845377c8151dd
Manuel Rey Laguarde <manuelreylaguarde@gmail.com>
2025-12-16 14:19:05 +0100

    commit 2

--- src/juegoWordle/PruebaEsqueleto.java
+++ src/juegoWordle/PruebaEsqueleto.java
@@ -3,7 +3,8 @@ package juegoWordle;
 public class PruebaEsqueleto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String [] palabras= {"carta","perro","salto","campo","norte","limon","dulce","trigo","plaza","freno","brazo",
+				"clavo","grito","silla","mundo","pesca","tarde","cinto","burro","saldo"};
 
 	}
 
